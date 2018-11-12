0b498fef90121617dc2b0e11ff1cd6dde8e49b74
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-12 03:37:12 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
   def keys_of(*arguments)
-    []
+    ret_array=[]
+    arguments.each do |arg|
+      self.each do |k, v|
+        if v == arg
+          ret_array.push(k)
+        end
+      end
+    end
   end
+  ret_array
 end
